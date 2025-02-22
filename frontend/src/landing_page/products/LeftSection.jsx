752b4294ac8bae2e0f510c3cffe930d5f5d88e47
Adarsh Kumar Pal <paladarsh978@gmail.com>
2025-02-22 23:45:09 +0530

    adding new file

---
+++ frontend/src/landing_page/products/LeftSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LeftSection() {
+  return (
+    <div>
+      <h2>left section</h2>
+    </div>
+  )
+}
+
+export default LeftSection
---
+++ frontend/src/landing_page/products/RightSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RightSection() {
+  return (
+    <div>
+      <h2>right section</h2>
+    </div>
+  )
+}
+
+export default RightSection
