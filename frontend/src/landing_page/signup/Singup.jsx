933fd2c640e6cc546d59057b20a84fcaacebb386
Adarsh Kumar Pal <paladarsh978@gmail.com>
2025-05-23 10:48:16 +0530

    steps to run folder

--- frontend/src/landing_page/signup/Singup.jsx
+++ frontend/src/landing_page/signup/Singup.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { useState } from 'react';
 
 function Singup() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Data:', formData); // API call ke liye yeh part modify karenge
+  };
   return (
     <div>
-      <h2>Singup</h2> 
+      <div className="container d-flex justify-content-center m-3">
+      <form onSubmit={handleSubmit} className='m-4'>
+      <label className='p-3'>
+        Username:
+        <input type="text" name="username" value={formData.username} onChange={handleChange} required />
+      </label>
+      <br />
+      <label className='p-3'>
+        Email:
+        <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+      </label>
+      <br />
+      <label className='p-3'>
+        Password:
+        <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+      </label>
+      <br />
+      <button type="submit">Sign Up</button>
+    </form>
+      </div>
     </div>
   )
 }
---
+++ readMe.md
@@ -0,0 +1,6 @@
+Steps for run this project :
+1. Clone this project in your local system
+2. Install all the dependencies : npm install 
+3. To run the backend : nodemon index.js
+4. To run the dashboard : npm start
+5. To run the frontend : npm run dev
